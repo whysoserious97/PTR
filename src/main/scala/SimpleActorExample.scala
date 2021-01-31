1faed6971e9352ad60edc680916b0173e18dcf96
whysoserious97 <andrei.lesco@isa.utm.md>
2021-01-31 02:23:57 +0200

    Some data from route 1

--- src/main/scala/SimpleActorExample.scala
+++ src/main/scala/SimpleActorExample.scala
@@ -1,23 +1,35 @@
-import akka.actor.Actor
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding.Get
+import akka.http.scaladsl.model.sse.ServerSentEvent
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 
-class Translator extends Actor {
-  def receive = {
-    case word: String =>
-      // ... process message
-      val reply = word.toUpperCase
-      sender() ! reply // reply to the ask
-  }
-}
+
+
 
 object HttpClientSingleRequest {
+
   def main(args: Array[String]): Unit = {
-//    var html = Await.result(get("http://localhost:4000/tweets/1"), 10.seconds)
-//    println(html)
+    implicit val system = ActorSystem()
+    implicit val mat = ActorMaterializer()
 
-//    def func(x:Int) : Future[Int] = ???
-    val src = Source("localhost:4000")
-    println(src)
+    import akka.http.scaladsl.unmarshalling.sse.EventStreamUnmarshalling._
+    import system.dispatcher
+    Http()
+      .singleRequest(Get(s"http://localhost:4000/tweets/1"))
+      .flatMap(Unmarshal(_).to[Source[ServerSentEvent, NotUsed]])
+      .foreach(source => source.runForeach(elem => println(prettyPrint(elem)) ))
   }
-}
 
+  def prettyPrint(event:ServerSentEvent):String =
+    s"""
+       |ID: ${event.id.getOrElse("No ID")}
+       |DATA: ${event.data}
+       |EVENT_TYPE: ${event.eventType.getOrElse("?")}
+       |RETRY: ${event.retry.getOrElse("")}
+    """.stripMargin
+}
